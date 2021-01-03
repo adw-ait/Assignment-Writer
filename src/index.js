4c17a65c1c7e57a422cace97ed3a25bba69525cf
adw-ait <adwaityugu@gmail.com>
2021-01-03 15:04:52 +0530

    font.css made

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./style.css";
-
+import "./fonts.css";
 ReactDOM.render(
   <React.StrictMode>
     <App />
