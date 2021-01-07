c83496ed45e53ce887c60971b5f61d4aa235444c
adw-ait <adwaityugu@gmail.com>
2021-01-07 20:28:29 +0530

    fixed different size image download

--- src/DownloadPage.js
+++ src/DownloadPage.js
@@ -5,11 +5,18 @@ import FileSaver from "file-saver";
 function DownloadPage() {
   const handleDownload = () => {
     window.scrollTo(0, 0);
-    html2canvas(document.querySelector("#capture")).then((canvas) => {
-      canvas.toBlob((blob) => {
-        FileSaver.saveAs(blob, "Assignment_screenshot.png");
-      });
-    });
+    let el = document.querySelector("#capture");
+    let cloned = el.cloneNode(true);
+    cloned.style.display = "block";
+    document.body.appendChild(cloned);
+    html2canvas(cloned, { height: 1122.519685, width: 793.700787 }).then(
+      (canvas) => {
+        canvas.toBlob((blob) => {
+          FileSaver.saveAs(blob, "Assignemnt_Screenshot.png");
+        });
+        cloned.style.display = "none";
+      }
+    );
   };
   return (
     <button
